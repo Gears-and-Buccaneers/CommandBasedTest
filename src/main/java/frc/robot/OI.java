707a88a9294dfcebd57b458bb29fadd8bc5df2c7
Jonah <jonah_lefkoff@hotmail.com>
2019-10-12 10:01:53 -0600

    cleaned up code & added all toggle commands to OI

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -7,16 +7,21 @@
 
 package frc.robot;
 
-
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
 import frc.robot.commands.ExtendDonger;
 import frc.robot.commands.RetractDonger;
+import frc.robot.commands.OpenDonger;
+import frc.robot.commands.CloseDonger;
 import frc.robot.commands.RaiseRamp;
 import frc.robot.commands.LowerRamp;
 import frc.robot.commands.RaiseGate;
 import frc.robot.commands.LowerGate;
+import frc.robot.commands.DeployClimber;
+import frc.robot.commands.RetractClimber;
+
 import frc.robot.commands.ToggleCommand;
 
 /**
@@ -25,14 +30,6 @@ import frc.robot.commands.ToggleCommand;
  */
 public class OI {
 
-  //Joystick pad;
-
-  //pad = new Joystick(0);
-  //// CREATING BUTTONS
-  // One type of button is a joystick button which is any button on a
-  //// joystick.
-  // You create one by telling it which joystick it's on and which button
-  // number it is.
   public Joystick pad = new Joystick(RobotMap.OI_DRIVER_CONTROLLER);
 
   double throttle = pad.getRawAxis(RobotMap.DRIVER_CONTROLLER_MOVE_AXIS); // Left JoyStick Vert.
@@ -50,21 +47,22 @@ public class OI {
   // double forceGate = pad.getRawAxis(3);
   // double time = System.currentTimeMillis();
 
-  Button D1 = new JoystickButton(pad, 1);
-	Button D2 = new JoystickButton(pad, 2);
-	Button D3 = new JoystickButton(pad, 3);
-	Button D4 = new JoystickButton(pad, 4);
-	Button D5 = new JoystickButton(pad, 5);
-	Button D6 = new JoystickButton(pad, 6);
-	Button D7 = new JoystickButton(pad, 7);
-	Button D8 = new JoystickButton(pad, 8);
-	Button D9 = new JoystickButton(pad, 9);
+  Button D1 = new JoystickButton(pad, 1); // x/a
+	Button D2 = new JoystickButton(pad, 2); // not used
+	Button D3 = new JoystickButton(pad, 3); // □/x
+	Button D4 = new JoystickButton(pad, 4); // △/y
+	Button D5 = new JoystickButton(pad, 5); // L1/LB
+	Button D6 = new JoystickButton(pad, 6); // R1/RB
+	Button D7 = new JoystickButton(pad, 7); // Share/Back
+	Button D8 = new JoystickButton(pad, 8); // Options/Start
 
 
   public OI() {
     //boolean isOpen = false;
     D6.whenPressed(new ToggleCommand(new ExtendDonger(0.2), new RetractDonger(0.2)));
+    D3.whenPressed(new ToggleCommand(new OpenDonger(), new CloseDonger()));
     D8.whenPressed(new ToggleCommand(new RaiseRamp(), new LowerRamp()));
-    D5.whenPressed(new ToggleCommand(new RaiseGate(0.15), new LowerGate(0.15)));
+    D5.whenPressed(new ToggleCommand(new RaiseGate(0.12), new LowerGate(0.12)));
+    D1.whenPressed(new ToggleCommand(new DeployClimber(), new RetractClimber()));
   }
 }
